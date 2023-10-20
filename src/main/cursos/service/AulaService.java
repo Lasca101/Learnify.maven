473e73d5bba0cce1e7e538f59a29f1b72f578877
henriquerlara <henriquerlara@outlook.com>
2023-10-20 11:09:54 -0300

    Update AulaService.java

--- src/main/cursos/service/AulaService.java
+++ src/main/cursos/service/AulaService.java
@@ -27,16 +27,16 @@ public class AulaService {
         Aula[] aulas = aulaDAO.getAulas();
         StringBuilder htmlAulas = new StringBuilder();
 
-        htmlAulas.append("<table border=\"1\">\n"); // Abre a tabela
+        htmlAulas.append("<table border=\"1\">\n"); 
 
         for (Aula aula : aulas) {
             String thumbnailBase64 = aula.getThumbnail(); // Já é uma string no formato Base64
 
-            htmlAulas.append("<tr>\n"); // Abre uma linha da tabela
+            htmlAulas.append("<tr>\n"); 
             htmlAulas.append("    <td><img src=\"data:image/jpeg;base64," + thumbnailBase64 + "\" alt=\"aula\"></td>\n");
             htmlAulas.append("    <td>" + aula.getTitulo() + "</td>\n");
             htmlAulas.append("    <td><a href=\"/pagina-aula/pagina_aula.html?id=" + aula.getIdAula() + "\">Ver Detalhes</a></td>\n");
-            htmlAulas.append("</tr>\n"); // Fecha a linha da tabela
+            htmlAulas.append("</tr>\n"); 
         }
 
         htmlAulas.append("</table>"); // Fecha a tabela
